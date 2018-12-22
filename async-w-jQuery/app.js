942d8077a29054d7f3e10dd70ce2c9b01eeb36a6
Patrick <torralbapatrick15@gmail.com>
2018-12-22 12:17:19 -0600

    Add async-w-jQuery

---
+++ async-w-jQuery/app.js
@@ -0,0 +1,68 @@
+/* eslint-env jquery */
+
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+
+        $.ajax({
+        	url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        	headers: {
+        		Authorization: 'Client-ID 9cdf400171389d03f96ad7b5852181dd72d4478dfd7975c0e98d4702923540a1'
+        	}
+        }).done(addImage)
+        .fail(function (err) {
+        	requestError(err, 'image');
+        });
+
+        $.ajax({
+        	url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=e64e14b0fe134df3911e18aa871b47e7`,
+        }).done(addArticles)
+        .fail(function (err) {
+        	requestError(err, 'article');
+        });
+    });
+
+    function addImage(data) {
+        let htmlContent = '';
+
+        if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+		        	<img src="${firstImage.urls.regular}" alt="${searchedForText}">
+		        	<figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+	        	</figure>`;
+        } else {
+            htmlContent = '<div class="error-no-image">No images available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    }
+
+    function addArticles(data) {
+        let htmlContent = '';
+
+        if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+	            	<h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+	            	<p>${article.snippet}</p>
+	            </li>`
+            ).join('') + '</ul>';
+        } else {
+            htmlContent = '<div class="error-no-image">No articles available</div>';
+        }
+
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(e, part) {
+	    console.log(e);
+	    responseContainer.insertAdjacentHTML('beforeend', `<p class="network-warning">Oh no! There was an error making a request for the ${part}.</p>`);
+	}
+})();
